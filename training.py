adcf441d46fdf16c36c0ac72924fb5e197c53883
Artyom <arxartyom@gmail.com>
2017-12-01 21:16:41 -0600

    full learning model logreg -> rnn -> rnn forest by boosting

---
+++ training.py
@@ -0,0 +1,5 @@
+#here is the full training model
+#current idea is rnn forest weited by boosting
+#each rnn generates by different initial seed and parameters
+#the idea is to cover different extremas
+#up to discussion
