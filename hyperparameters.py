2a79e559b71d967435393a4a0f7bfb410bcdf58b
Artyom <arxartyom@gmail.com>
2017-12-06 20:22:02 -0600

    useless updates

--- hyperparameters.py
+++ hyperparameters.py
@@ -17,8 +17,34 @@ logreg_db = "logreg.data"       #saved trained logreg tensor
 ##
 #RNN pace hyperparameters
 ##
-learning_rates = [1e-6]         #set of learing rates
-momentums = [0.98]              #set of momentums
-batches = [200]                 #set of batch sizes
-depths = [depth]                #set of depthes 
-activations = [T.nnet.softmax]  #set of activation functions for hidden layer
+
+
+def empty(X):
+    return X
+
+def inv_empty(X):
+    return np.ones((X.shape[0],X.shape[1]))
+
+
+def sigmoid(X):
+    return np.exp(X) / (1 + np.exp(X))
+
+
+def inv_sigmoid(X):
+    return X * (1 - X)
+
+
+def softmax(X):
+    v = X - np.amax(X, axis=1)[:, None]
+    exp = np.exp(v)
+    return exp / np.sum(exp, axis=1)[:, None]
+
+
+learning_rates = [1e-6, 1e-4,1e-2,1e0,1e2]          #set of learing rates
+activations = [empty, sigmoid, softmax]             #set of activation functions for hidden layer
+inv_activations = [inv_empty, sigmoid, softmax]
+num_neurals = [1024, 2048, 4096]
+limit = 5000
+###
+# logreg hyperparameters
+###
