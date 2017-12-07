44bc573a60f8ead35dbf95c0c8b1fe7ff2b9246d
Artyom <arxartyom@gmail.com>
2017-12-06 22:49:24 -0600

    ml integrated + output

--- start.py
+++ start.py
@@ -1,47 +1,52 @@
-#this is the main file that grate\instructor will run to repeat experience
+# this is the main file that grate\instructor will run to repeat experience
 
 
-#12/2 SAMPLE CODE
+# 12/2 SAMPLE CODE
 
-#there are 2 methods that can be run
-#train -- train the model and saves at destination(see def below)
-#show_result -- use data from destination to show learning results
+# there are 2 methods that can be run
+# train -- train the model and saves at destination(see def below)
+# show_result -- use data from destination to show learning results
 
 
-import matplotlib.pyplot as plt
-import rnnLearning
-import sortInfo
-import training
-import logregBlockLearning
-import hyperparameters
+import sortInfo as si
+import logregBlockLearning as lg
+import hyperparameters as hp
+import heatmap as hm
+import SNN
 
-#source with db
-#source = open("Business_Licenses.csv","rb")
-source = open(hyperparameters.db,"rb")
-#destination to store the data
-destination = open("model_results.data","w")
+# source with db
+# source = open("Business_Licenses.csv","rb")
+source = open(hyperparameters.db, "rb")
+# destination to store the data
+destination = open("model_results.data", "w")
+
 
 ##
-#end of hyperparameters
+# end of hyperparameters
 ##
 
-def train:
+def train():
     "perform traning"
-    #1, formate the data by adding extra parameters i.e. (x,y) quazilocations
-    #see sortInfo
-    
-    #2, sort data by time frame
+    # 1, formate the data by adding extra parameters i.e. (x,y) quazilocations
+    # see sortInfo
+    si.fix_data(hp.db)
+    # 2, sort data by time frame
 
-    #3, convert data to block using logReg or what ever model is defined
+    # 3, convert data to block using logReg or what ever model is defined
+    data = []
+    X = []
+    Y = []
+    last = []
+    # 4, train rnn rf based on blocks with time frames and save results
+    forest = SNN.forest(X, Y, last)
+    # 5, train logreg to weight each RNN properly
+    model, y = lg.train_forest(forest, X, Y, last)
+    # 6, show result
+    hm.create_heat(y)
 
-    #4, train rnn rf based on blocks with time frames and save results
-
-    #5, train logreg to weight each RNN properly
-
-def show_result:
-    "parse bac from destination and draw heat maps using .. plt
-    #there are 2 possible heat maps
-    #1. by chicago businesses density
-    #2. by superior business type density (relative or absolute)
-    #question for discussion
-    
+# def show_result():
+# "parse bac from destination and draw heat maps using .. plt
+# there are 2 possible heat maps
+# 1. by chicago businesses density
+# 2. by superior business type density (relative or absolute)
+# question for discussion
